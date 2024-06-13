d64678a668343148bafde31e19d1e62f5454e489
zachary <>
2024-06-12 23:24:50 -0500

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
-import './style.css'
+import {
+    Quasar,
+    LocalStorage,
+    SessionStorage
+} from 'quasar'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+const app = createApp(App);
+
+app.use(Quasar, {
+    config: {
+        dark: 'auto',
+    },
+    plugins: {
+        LocalStorage,
+        SessionStorage
+    }, // import Quasar plugins and add here
+})
+
+app.mount('#app');
